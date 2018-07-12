d0365bac552dded3378e965b55848b5a54d3d2fb
Keegan Kelly <keegan.kelly@scientificgames.com>
2018-07-12 10:40:22 -0500

    Added function that generates random color and displays it on the client

--- app/summon.php
+++ app/summon.php
@@ -1,5 +1,12 @@
 <?php
 
-echo "blue <br />";
+
+$hex = "#" . dechex(mt_rand(0, 0xFFFFFF));
+
+echo "<div class='panel'>
+        <div class='panel-body' style='background-color:$hex'>
+        </div>
+      </div>";
+
 
  ?>
