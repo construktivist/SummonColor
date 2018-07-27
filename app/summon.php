b717d3c5ed2791fc9ceeabad11b5b7fc2521e752
Keegan Kelly <keegan.kelly@scientificgames.com>
2018-07-27 09:10:26 -0500

    Added summon functions into a class

--- app/summon.php
+++ app/summon.php
@@ -1,17 +1,34 @@
 <?php
 
+Class SummonColor
+{
 
-$hex = "#" . dechex(mt_rand(0, 0xFFFFFF));
+  private $hex;
 
-echo "<div  class='panel' style=
-            'background-color:$hex;
-             width:64px;
-             height:64px;
-             margin:5px;
-             float:left;'>
-        <div class='panel-body'>
-        </div>
-      </div>";
+  private function getHex(){
+    $this->hex = "#" . dechex(mt_rand(0, 0xFFFFFF));
+  }
 
+  private function echoColor(){
+    echo "<div  class='panel' style=
+              'background-color:$this->hex;
+               width:64px;
+               height:64px;
+               margin:5px;
+               float:left;'>
+          <div class='panel-body'>
+          </div>
+        </div>";
+  }
+
+  public function getAndDisplayColor(){
+    $this->getHex();
+    $this->echoColor();
+  }
+
+}
+
+$summonColor = new SummonColor();
+$summonColor->getAndDisplayColor();
 
  ?>
